dd072f1f909d1515cfdb1e2d4ce91d506a1ad46d
Alejo Gschwind <alejogschwind.97@gmail.com>
2020-06-14 17:51:49 -0300

    Updates

---
+++ precache-manifest.02ed829e6988b244fbaf80528bf04d32.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fcd37e1781488033c93c189f8e881f",
+    "url": "/mapins/index.html"
+  },
+  {
+    "revision": "49b1b8b5885533aef95c",
+    "url": "/mapins/static/css/main.a17eb83a.chunk.css"
+  },
+  {
+    "revision": "b0c1d05f370349da2431",
+    "url": "/mapins/static/js/2.eb12b0c0.chunk.js"
+  },
+  {
+    "revision": "ee45fe4bc90ffb536db5566b15993c1a",
+    "url": "/mapins/static/js/2.eb12b0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b1b8b5885533aef95c",
+    "url": "/mapins/static/js/main.a608c967.chunk.js"
+  },
+  {
+    "revision": "2e5947d8c560cc4c24c1",
+    "url": "/mapins/static/js/runtime-main.5b4438f8.js"
+  },
+  {
+    "revision": "85e49c20fe4ef6cc3dc6889928fc2d51",
+    "url": "/mapins/static/media/arrowdown.85e49c20.svg"
+  },
+  {
+    "revision": "84f724693f1462eb92b7f12ef64dc344",
+    "url": "/mapins/static/media/close.84f72469.svg"
+  },
+  {
+    "revision": "2bd1dbcfb1fd26590bb726e8313283e0",
+    "url": "/mapins/static/media/marker.2bd1dbcf.svg"
+  }
+]);
